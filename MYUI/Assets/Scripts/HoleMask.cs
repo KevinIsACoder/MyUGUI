eef8d774fb38f88676153d3aafb8881167757d25
KL-20200730BDQX\Administrator <1106169797@qq.com>
2023-03-29 11:12:21 +0800

    添加内置shader

--- MYUI/Assets/Scripts/HoleMask.cs
+++ MYUI/Assets/Scripts/HoleMask.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class HoleMask : MonoBehaviour
 {
+    public GameObject canvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var rt = GetComponent<RectTransform>();
+        Vector3[] v3 = new Vector3[4];
+        rt.GetWorldCorners(v3);
+        var canvasTransform = canvas.GetComponent<Transform>();
+        for (int i = 0; i < 4; ++i)
+            v3[i] = canvasTransform.InverseTransformPoint(v3[i]);
+        foreach (var VARIABLE in v3)
+        {
+            Debug.Log(VARIABLE);     
+        }
     }
 
     // Update is called once per frame
